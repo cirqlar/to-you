1eae41f5bf064616bcc57862494ec462efb0a028
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2020-08-04 18:45:10 +0100

    Mostly working:

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 Vue.component('todo-form', {
   data: function() {
     return {
-      id: 74,
+      id: null,
       title: "",
     }
   },
   methods: {
-    sub() {
+    sub(event) {
       this.$emit('submit', this.todo);
+      event.target.reset();
     }
   },
   computed: {
@@ -20,21 +21,60 @@ Vue.component('todo-form', {
   },
   template: `
     <form v-on:submit.prevent="sub">
-      This works :) {{ id }}
       <input v-model="title" type="text"/>
       <input type="submit" value="Submit meeeee"/>
     </form>
   `
 });
 
+Vue.component('todo', {
+  data: function() {
+    return {
+      placeholder: 1,
+    }
+  }
+  ,
+  props: ['todo'],
+  template: `
+    <div>
+      <h1>{{ todo.title }}</h1>
+      <button v-on:click.prevent="$emit('destroy', todo.id)">Delete</button>
+    </div>
+  `
+})
+
+Vue.component('todo-list', {
+  data: function() {
+    return {
+      placeholder: 1,
+    }
+  },
+  props: ['todos', 'destroy'],
+  template: `
+    <div>
+      <todo v-for="todo in todos" v-bind:todo='todo' v-on:destroy="destroy" v-bind:key="todo.id"></todo>
+    </div>
+  `
+})
+
 let app = new Vue({
   el: '#app',
   data: {
-    message: "We made it!! :D"
+    todos: [],
   },
   methods: {
     submit(data) {
-      console.log(data)
-    }
+      if (!data.id) {
+        data.id = this.todos.length;
+      }
+      this.todos.push(data);
+    },
+    destroy(todo) {
+      const index = this.todos.findIndex( el => el.id == todo );
+
+      if (index > -1) {
+        this.todos.splice(index, 1);
+      }
+    },
   },
 });
