488933fa858d39fd0063f1e74e4f234ba7d9f742
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2020-08-04 18:00:39 +0100

    Working form

--- script.js
+++ script.js
@@ -2,12 +2,28 @@ Vue.component('todo-form', {
   data: function() {
     return {
       id: 74,
+      title: "",
+    }
+  },
+  methods: {
+    sub() {
+      this.$emit('submit', this.todo);
+    }
+  },
+  computed: {
+    todo: function() {
+      return {
+        title: this.title,
+        id: this.id,
+      }
     }
   },
   template: `
-    <div>
+    <form v-on:submit.prevent="sub">
       This works :) {{ id }}
-    </div>
+      <input v-model="title" type="text"/>
+      <input type="submit" value="Submit meeeee"/>
+    </form>
   `
 });
 
@@ -15,5 +31,10 @@ let app = new Vue({
   el: '#app',
   data: {
     message: "We made it!! :D"
-  }
+  },
+  methods: {
+    submit(data) {
+      console.log(data)
+    }
+  },
 });
