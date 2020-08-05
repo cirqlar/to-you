a745a1f2297c90f913471700182511afeb062def
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2020-08-05 17:15:36 +0100

    Finish categories and stuff

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ Vue.component('todo-form', {
       id: null,
       title: "",
       date: (new Date()).toISOString().substr(0,10),
+      category: 'default',
       errors: [],
       disabled: true,
     }
   },
+  props: ['categories'],
   methods: {
     sub(event) {
       if (this.title != "") {
@@ -36,6 +38,7 @@ Vue.component('todo-form', {
         title: this.title,
         id: this.id,
         date: this.date,
+        category: this.category,
       }
     }
   },
@@ -52,8 +55,13 @@ Vue.component('todo-form', {
         v-model="title"
         placeholder="Type something..."
         type="text"/>
+      <select
+        class="w-full flex-none border rounded mt-4 px-3 py-2 bg-white focus:border-blue-500 transition-colors duration-150 outline-none sm:w-auto sm:flex-auto"
+        v-model="category">
+        <option v-for='(color, name) in categories' v-bind:value='name'>{{name}}</option>
+      </select>
       <input
-        class='flex-auto mt-4 border rounded px-3 py-2 focus:border-blue-500 transition-colors duration-150 outline-none'
+        class='flex-auto mt-4 border rounded px-3 py-2 focus:border-blue-500 transition-colors duration-150 outline-none sm:ml-2'
         v-model="date"
         type="date"/>
       <input
@@ -65,15 +73,78 @@ Vue.component('todo-form', {
   `
 });
 
+Vue.component('category-item', {
+  data: function() {
+    return {
+
+    }
+  },
+  props: ['name', 'color', 'active'],
+  computed: {
+    bg: function() {
+      let strength = this.active ? '500' : '300';
+      return `bg-${this.color}-${strength} hover:bg-${this.color}-700 focus:bg-${this.color}-700`;
+    }
+  },
+  template: `
+    <button
+      v-bind:class="[bg, 'text-sm px-2 py-1 rounded focus:outline-none']"
+      v-on:click.prevent="$emit('click')"
+      >
+      {{name}}
+    </button>
+  `
+})
+
+Vue.component('category-list', {
+  data: function() {
+    return {
+      active: "",
+    }
+  },
+  methods: {
+    click(name) {
+      if (this.active == name) {
+        name = "";
+      }
+      this.active = name;
+      this.$emit('filter', name);
+    }
+  },
+  props: ['categories'],
+  template: `
+    <div class="px-4 py-2 border-b overflow-x-auto">
+      <div class="inline-flex items-center">
+        <span class=''>Categories: </span>
+        <category-item
+          v-for='(color, name) in categories'
+          class='ml-2'
+          v-bind:key="name"
+          v-bind:color="color"
+          v-bind:name="name"
+          v-bind:active=" active == '' || active == name"
+          v-on:click='click(name)'
+          >
+        </category-item>
+      </div>
+    </div>
+  `
+})
+
 Vue.component('todo', {
   data: function() {
     return {
     }
-  }
-  ,
-  props: ['todo'],
+  },
+  computed: {
+    border: function() {
+      return `border-${this.color}-500`;
+    }
+  },
+  props: ['todo', 'color'],
   template: `
-    <div>
+    <div
+      v-bind:class="[border, 'border-l-4']">
       <h1>{{ todo.title }}</h1>
       <button v-on:click.prevent="$emit('destroy', todo.id)">Delete</button>
     </div>
@@ -85,10 +156,18 @@ Vue.component('todo-list', {
     return {
     }
   },
-  props: ['todos', 'destroy'],
+  props: ['todos', 'destroy', 'categories'],
   template: `
-    <div v-if='todos.length > 0'>
-      <todo v-for="todo in todos" v-bind:todo='todo' v-on:destroy="destroy" v-bind:key="todo.id"></todo>
+    <div
+      class="grid template-auto-fill w-full justify-center mx-auto p-4 gap-3"
+      v-if='todos.length > 0'>
+      <todo
+        v-for="todo in todos"
+        v-bind:todo='todo'
+        v-on:destroy="destroy"
+        v-bind:color="categories[todo.category]"
+        v-bind:key="todo.id">
+      </todo>
     </div>
     <div
       class="w-full h-full text-center flex justify-center items-center text-gray-600"
@@ -102,6 +181,17 @@ let app = new Vue({
   el: '#app',
   data: {
     todos: [],
+    categories: {
+      'default': 'blue',
+    },
+    filter: '',
+  },
+  computed: {
+    filteredTodos: function() {
+      if (this.filter == "")
+        return this.todos;
+      return this.todos.filter(x => x.category == this.filter)
+    }
   },
   methods: {
     submit(data) {
@@ -117,5 +207,8 @@ let app = new Vue({
         this.todos.splice(index, 1);
       }
     },
+    filt(name) {
+      this.filter = name;
+    }
   },
 });
